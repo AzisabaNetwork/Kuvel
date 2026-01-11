cacda4983407395541459d3099e1923dbab66650
acrylic-style <me@acrylicstyle.xyz>
2026-01-11 16:15:32 +0900

    bump version

--- src/main/java/net/azisaba/kuvel/Kuvel.java
+++ src/main/java/net/azisaba/kuvel/Kuvel.java
@@ -30,7 +30,7 @@ import org.slf4j.Logger;
 @Plugin(
     id = "kuvel",
     name = "Kuvel",
-    version = "3.1.0-alpha.2",
+    version = "3.1.0-alpha.3",
     url = "https://github.com/AzisabaNetwork/Kuvel",
     description =
         "Server-discovery Velocity plugin for Minecraft servers running in a Kubernetes cluster.",
